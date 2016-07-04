8c3134850fbaa701e6346a1b2ca6e2b4d964db1a
Vladimir <V1ad1m1r@mail.com>
2016-07-04 03:50:30 -0500

    First Commit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treehouse.FitnessFrog
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            double runningTotal = 0;
+            while (true)
+            {
+
+
+                // Prompt User for minutes exercised
+                System.Console.Write("Enter how many minutes you exercised, or type quit to exit.");
+
+                var entry = System.Console.ReadLine();
+
+                if (entry.ToLower() == "quit")
+                {
+                    break;
+                }
+
+                try
+                {
+                    var minutes = double.Parse(entry);
+
+                    if (minutes <= 0)
+                    {
+                        System.Console.WriteLine("Enter a number!!!");
+                        continue;
+                    }
+                    else if (minutes < 10)
+                    {
+                        System.Console.WriteLine("Better than nothing!");
+                    }
+                    else if (minutes < 30)
+                    {
+                        System.Console.WriteLine("Way To Go!!");
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("YOUR A MACHINE!!!");
+                    }
+
+                    // Add minutes to get running total
+                    runningTotal += minutes;
+
+                    //display to screen
+
+                    System.Console.WriteLine("You've exercised " + runningTotal + " minutes");
+                }
+                catch (System.Exception)
+                {
+                    System.Console.WriteLine("Enter a number!!!");
+                    continue;
+                }
+
+
+
+
+                // Repeat until user quits
+            }
+
+        }
+
+    }
+}
